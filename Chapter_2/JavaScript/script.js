75250e5d4f199a0da72632d342717f1dd914a89e
devpovoa <thiagopovoadev@hotmail.com>
2024-06-18 20:54:04 -0300

    created new html and script file

--- Chapter_2/JavaScript/script.js
+++ Chapter_2/JavaScript/script.js
@@ -1,9 +1,31 @@
 "use strict";
 
-function showHello() {
-    let name = document.getElementById("inNome").value;
-    document.getElementById("outResponse").textContent = "Hello, " + name + " !";
+// Mostra nome.
+// function showHello() {
+//     let name = document.getElementById("inNome").value;
+//     document.getElementById("outResponse").textContent = "Hello, " + name + " !";
+// }
+
+// let show = document.getElementById("btShow");
+// show.addEventListener("click", showHello);
+
+// Vídeo Locadora
+
+function convertDuration() {
+    const MINUTES = 60;
+    let title = document.getElementById("inTitle").value;
+    let duration = Number(document.getElementById("inDuration").value);
+
+    let hours = Math.floor(duration / MINUTES);
+    let minutes = duration % MINUTES;
+
+    document.querySelector('.outTitle').textContent = title;
+    document.querySelector('.outResponse').textContent = hours + " hours and " + minutes + " minutes ";
 }
 
-let show = document.getElementById("btShow");
-show.addEventListener("click", showHello);
+let btConvert = document.getElementById("btConvert").addEventListener("click", convertDuration);
+
+
+
+
+
